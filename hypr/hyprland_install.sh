cbeebf3e26359f3eb5846fea31f2682784c13808
Ferriit <ferriitalt@gmail.com>
2025-08-25 16:23:45 +0200

    Create hyprland_install.sh

---
+++ hypr/hyprland_install.sh
@@ -0,0 +1,13 @@
+# Core Hyprland stack
+sudo pacman -S hyprland swaybg swaylock
+# Or if you prefer, use hyprpaper for wallpapers
+sudo pacman -S hyprpaper
+
+# Bar
+sudo pacman -S waybar
+
+# Notification daemon
+sudo pacman -S mako
+
+# Lock screen
+sudo pacman -S hyprlock
